98affb50a6536631356cc0d759e5ea5ec33f0e33
dinailza09 <dinailza09@gmail.com>
2023-08-24 21:05:56 -0300

    Tipos de Variáveis

---
+++ type.js
@@ -0,0 +1,34 @@
+//NUMBER
+const primeironumero = 3;
+const segundonumero = 4;
+const soma = primeironumero + segundonumero;
+console.log(soma);
+
+//STRING - ASPAS DULPAS"" OU SIMPLES''
+const texto1 = "Olá mundo!";
+const texto2 = 'Olá mundo 2!';
+console.log(texto1);
+console.log(texto2);
+
+//Concatenação (+) UNIR
+const frase = "Meu nome é ";
+const nome = "Dinailza Soares!";
+console.log(frase + nome);
+
+//Quantos caracteres uma string contém:
+var senha = "Dina12345";
+console.log(senha.length);
+
+//TYPE Boolean (TRUE-Verdadeiro/FALSE-Falso)
+const numero1 = 5;
+const numero2 = 10;
+console.log(numero1 === numero2); //Retorna false, pq não são iguais!
+console.log(numero1 !== numero2); //Retorna true, pq são diferentes!
+
+const number1 = 5;
+const number2 = 5;
+console.log(number1 === number2); //Retorna true, pq são iguais e do mesmo tipo!
+
+// = Atribuindo Valor
+// == Comparando Valores
+// === Comparando valores idênticos e do mesmo tipo!
