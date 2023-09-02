2431d1feaccde766d7719db748861b87877c787e
dinailza09 <dinailza09@gmail.com>
2023-09-02 19:16:07 -0300

    aprendizado

--- conversoes.js
+++ conversoes.js
@@ -13,3 +13,22 @@ console.log(numero==numeroString);//true, pq ele não compara o tipo!
 let numeroString2= Number('300');//converte para number
 console.log(typeof(numeroString2));//retorna do tipo number
 
+
+//Existe vários tipos de console
+console.log('Teste');//para exibir  no console
+console.error('Deu erro');//para tratamento de erros 
+
+//Operador ternário
+const idademinima= 18;
+const idadecliente= 20;
+  //              Condicao                True        False
+console.log(idadecliente>=idademinima ? 'Cerveja' : 'Suco');
+
+const nome = "Leo";
+const idade = 23;
+const bebidaMaior = "cerveja";
+const bebidaMenor = "suco";
+
+const pedido = `${nome} diz: "por favor, quero beber ${idade >= 18 ? bebidaMaior : bebidaMenor}"`
+console.log(pedido);
+
---
+++ funcoes.js
@@ -0,0 +1,16 @@
+// declaração de função
+
+ //      nomedafuncao //paramêtro
+function imprimeTexto(texto){
+    console.log(texto);
+}
+imprimeTexto('Oi');
+
+//função sem paramêtro
+function soma(){
+    return 2+2;
+}
+console.log(soma());
+
+//três formas de escrever funções!
+
